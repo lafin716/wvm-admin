4b7aa54e67ae96f65c91a9579f795a7a5c0ac3a2
lafin716 <lafin716@gmail.com>
2022-09-16 03:02:43 +0900

    init commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,93 @@
+// ** Icon imports
+import Login from 'mdi-material-ui/Login'
+import Table from 'mdi-material-ui/Table'
+import CubeOutline from 'mdi-material-ui/CubeOutline'
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
+import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
+import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
+import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
+import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
+import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import { BellBadgeOutline, CloudUploadOutline, SemanticWeb, SquareRoundedOutline } from 'mdi-material-ui'
+
+const navigation = () => {
+  return [
+    {
+      title: '대시보드',
+      icon: HomeOutline,
+      path: '/'
+    },
+    {
+      title: '계정설정',
+      icon: AccountCogOutline,
+      path: '/account-settings'
+    },
+    {
+      title: '앱 관리',
+      icon: SemanticWeb,
+      path: '/pages/login'
+    },
+    {
+      title: '배포 관리',
+      icon: CloudUploadOutline,
+      path: '/pages/login'
+    },
+    {
+      title: '푸시 관리',
+      icon: BellBadgeOutline,
+      path: '/pages/login'
+    },
+    {
+      sectionTitle: 'Pages'
+    },
+    {
+      title: 'Login',
+      icon: Login,
+      path: '/pages/login',
+      openInNewTab: true
+    },
+    {
+      title: 'Register',
+      icon: AccountPlusOutline,
+      path: '/pages/register',
+      openInNewTab: true
+    },
+    {
+      title: 'Error',
+      icon: AlertCircleOutline,
+      path: '/pages/error',
+      openInNewTab: true
+    },
+    {
+      sectionTitle: 'User Interface'
+    },
+    {
+      title: 'Typography',
+      icon: FormatLetterCase,
+      path: '/typography'
+    },
+    {
+      title: 'Icons',
+      path: '/icons',
+      icon: GoogleCirclesExtended
+    },
+    {
+      title: 'Cards',
+      icon: CreditCardOutline,
+      path: '/cards'
+    },
+    {
+      title: 'Tables',
+      icon: Table,
+      path: '/tables'
+    },
+    {
+      icon: CubeOutline,
+      title: 'Form Layouts',
+      path: '/form-layouts'
+    }
+  ]
+}
+
+export default navigation
